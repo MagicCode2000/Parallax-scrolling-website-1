9a01514e573f655069d85b483bff42fdfb3cb254
MagicCode2000 <147378149+MagicCode2000@users.noreply.github.com>
2023-10-09 15:07:22 +0330

    Parallax-scrolling-website-1

---
+++ main.js
@@ -0,0 +1,4 @@
+window.addEventListener("scroll",(e)=>{
+    document.querySelector(".background").style.backgroundSize = `${window.scrollY * 1.8 + 1600}px`
+    document.querySelector("#H1").style.opacity = `${(-window.scrollY + 300) * .004}`
+})
